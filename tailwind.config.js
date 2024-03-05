376e7bec1c7548a88ec10f84ac91314ed3a1d835
Eugene <evgenij.kulikov@gmail.com>
2024-03-05 19:19:51 +0100

    Adds style-guide variables to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      // Primary
+      marineBlue: "hsl(213, 96%, 18%)",
+      purplishBlue: "hsl(243, 100%, 62%)",
+      pastelBlue: "hsl(228, 100%, 84%)",
+      lightBlue: "hsl(206, 94%, 87%)",
+      strawberryRed: "hsl(354, 84%, 57%)",
+      // Neutral
+      coolGray: "hsl(231, 11%, 63%)",
+      lightGray: "hsl(229, 24%, 87%)",
+      magnolia: "hsl(217, 100%, 97%)",
+      alabaster: "hsl(231, 100%, 99%)",
+      white: "hsl(0, 0%, 100%)",
+    },
     extend: {
+      screens: {
+        xs: "375px",
+        xl: "1440px",
+      },
       gridTemplateAreas: {
         "form-desktop": ["nav form form", "nav form form", "nav footer footer"],
       },
